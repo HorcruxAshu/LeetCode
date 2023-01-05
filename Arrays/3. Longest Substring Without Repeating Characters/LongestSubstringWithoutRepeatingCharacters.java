6b26651b68e997a75f84a20ad480df59b810a84d
Ashutosh Manorkar <50377125+HorcruxAshu@users.noreply.github.com>
2023-01-05 19:29:44 +0530

    Brute

---
+++ Arrays/3. Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,22 @@
+//Approach is to set two counters (a, b) where if a same character is found increment a, else b
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null) return 0;
+        Set<Character> set = new HashSet<>();
+        int maxLen = 0;
+        int b = 0;
+        int a = 0;
+        while(b < s.length()){
+            if(!(set.contains(s.charAt(b)))){
+                set.add(s.charAt(b));
+                maxLen = Math.max(maxLen, set.size());
+                b++;
+            }else{
+                set.remove(s.charAt(a));
+                a++;
+            }
+        }
+        return maxLen;
+    }
+}
