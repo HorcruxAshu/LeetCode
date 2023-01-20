6724df1a088a6d976303cd9f68884eece6f4e597
Ashutosh Manorkar <50377125+HorcruxAshu@users.noreply.github.com>
2023-01-20 13:57:30 +0530

    Most Optimal Approach

--- LinkedList/160. Intersection of Two Linked Lists.java
+++ LinkedList/160. Intersection of Two Linked Lists.java
@@ -9,37 +9,20 @@ class ListNode {
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
        
+        //Most Optimal Approach
+
+        //T.C = O(2m)
+        //S.C = O(1)
+
+        if(headA == null || headB == null) return null;
         ListNode tempA = headA;
         ListNode tempB = headB;
-        ListNode runA = headA;
-        ListNode runB = headB;
-        int sizeA = 0;
-        int sizeB = 0;
-        while(tempA != null || tempB != null){
-            if(tempA != null){tempA = tempA.next; sizeA++;}
-            if(tempB != null) {tempB = tempB.next;sizeB++;}
-            
-        }
-        if(sizeA > sizeB){
-            int diff = sizeA - sizeB;
-            while(diff-->0){
-                runA = runA.next;
-            }
-        }else if(sizeA < sizeB){
-            int diff = sizeB - sizeA;
-            while(diff-->0){
-                runB = runB.next;
-            }
-        }
-        while(runA != null || runB != null){
-            if(runA == runB && runA.val != 0){
-                return runA;
-            } 
-            runA = runA.next;
-            runB = runB.next;
+      
+        while(tempA != tempB){
+            tempA = tempA == null ? headB: tempA.next;
+            tempB = tempB == null ? headA: tempB.next;
         }
         
-        // System.out.println(sizeA + " " + sizeB+ " " + runA.val + " " + runB.val);
-        return null;
+        return tempA;
     }
 }
